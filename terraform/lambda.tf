6dde13783cb511ff0ab2c13bc96fe2014fbeccf9
VitorFidelis <vitor.fidelis5587@gmail.com>
2026-02-09 10:34:16 -0300

    ci(infra): terraform deploy [apply]

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,11 +1,11 @@
 # Criaçao de uma funçao lambda na AWS
 resource "aws_lambda_function" "helloworld" {
     description = "Função Lambda Hello World - Infra base"
-    function_name = var.lambda_function_name
+    function_name = var.aws_lambda_function_name
     role          = data.aws_iam_role.lambda_role.arn
 
-    runtime = var.lambda_function_runtime
-    handler = var.lambda_function_handler
+    runtime = var.aws_lambda_function_runtime
+    handler = var.aws_lambda_function_handler
 
     filename = var.aws_lambda_function_filename
 
