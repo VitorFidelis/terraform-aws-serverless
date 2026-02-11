8aeacd2f102f3c25ffed53de1a269a66c1a9f50f
VitorFidelis <vitor.fidelis5587@gmail.com>
2026-02-11 13:59:48 -0300

    ci(infra): terraform deploy [apply]

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -14,7 +14,7 @@ resource "aws_lambda_function" "ms_medicamentos" {
       subnet_ids = data.aws_subnets.default.ids
 
       security_group_ids = [
-        aws_security_group.lambda_sg.id
+        data.aws_security_group.lambda_sg.id
       ]
     }
 
