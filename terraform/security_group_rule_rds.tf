8716726110333d733a9ed9a23188f18c026916f9
VitorFidelis <vitor.fidelis5587@gmail.com>
2026-02-11 14:05:17 -0300

    ci(infra): terraform deploy [apply]

--- terraform/security_group_rule_rds.tf
+++ terraform/security_group_rule_rds.tf
@@ -4,5 +4,5 @@ resource "aws_security_group_rule" "allow_lambda_to_rds" {
   to_port                  = 5432
   protocol                 = "tcp"
   security_group_id        = data.aws_security_group.rds_existing.id
-  source_security_group_id = aws_security_group.lambda_sg.id
+  source_security_group_id = data.aws_security_group.lambda_sg.id
 }
