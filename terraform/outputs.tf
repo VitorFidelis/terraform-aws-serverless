bfffc9538fc8fb5670c05ed9db84eee7003adc4f
VitorFidelis <vitor.fidelis5587@gmail.com>
2026-02-10 22:11:08 -0300

    ci(infra): terraform deploy [apply]

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,7 +6,7 @@ output "api_gateway_url" {
 
 output "lambda_function_name" {
   description = "Nome da função Lambda"
-  value       = aws_lambda_function.helloworld.function_name
+  value       = aws_lambda_function.ms_medicamentos.function_name
 }
 
 output "db_endpoint" {
